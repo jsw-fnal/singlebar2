32df9a8d4fa430b3c3aac713ce34509017f4e477
Bob Hirosky <hirosky@virginia.edu>
2021-11-11 08:25:01 -0500

    fix typo

--- run_mu.sh
+++ run_mu.sh
@@ -5,7 +5,7 @@ python runner.py -bmu- -G10 -n500 -omu_10GeV -d air_gap
 python runner.py -bmu- -G30 -n500 -omu_30GeV -d air_gap
 
 python runner.py -bmu- -M300 -n500 -omu_300MeV -g2 -d og_gap
-python runner.py -bmu- -G1 -n500 -omu_1GeV -d -g2 og_gap
-python runner.py -bmu- -G3 -n500 -omu_3GeV -d -g2 og_gap
+python runner.py -bmu- -G1 -n500 -omu_1GeV -g2 -d og_gap
+python runner.py -bmu- -G3 -n500 -omu_3GeV -g2 -d og_gap
 python runner.py -bmu- -G10 -n500 -omu_10GeV -g2 -d og_gap
 python runner.py -bmu- -G30 -n500 -omu_30GeV -g2 -d og_gap
